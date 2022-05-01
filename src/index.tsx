7523cce7f7e9358bfe4be2590c6b99cb42a11c2f
TrungHuynh <51523673+trunghuynh240@users.noreply.github.com>
2022-05-01 11:57:56 +0700

    First save

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const root = document.getElementById("root")!;
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+const render = (Component) =>
+	// eslint-disable-next-line react/no-render-return-value
+	ReactDOM.render(
+		<React.StrictMode>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</React.StrictMode>,
+		root
+	);
+
+render(App);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
