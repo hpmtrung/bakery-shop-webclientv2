7f8da581a3857547fabf84587c864de5fcf54f69
TrungHuynh <51523673+trunghuynh240@users.noreply.github.com>
2022-05-29 05:27:31 +0700

    Change account email setting to readonly

--- src/app/modules/account/settings/AccountSettingsPage.tsx
+++ src/app/modules/account/settings/AccountSettingsPage.tsx
@@ -143,6 +143,7 @@ export const AccountSettingsPage = () => {
 									required: "Your email is required",
 								}}
 								fullWidth
+								readOnly
 							/>
 						</Grid>
 						<Grid container item spacing={1}>
